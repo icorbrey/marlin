3ee091834517cf66f383ea3058f211283d46dec8
Isaac <icorbrey@gmail.com>
2022-07-13 02:09:25 -0400

    fix: Minus sign

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1670,7 +1670,7 @@
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
-#define Y_MIN_POS 2
+#define Y_MIN_POS -2
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
